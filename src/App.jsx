557cb19d480540e512b8d14ee99a54dd22acf812
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-15 21:09:01 +0600

    Json Data Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import Banner from "./assets/Components/Banner/Banner"
 import Cart from "./assets/Components/Cart/Cart"
 import Navbar from "./assets/Components/Navbar/Navbar"
 import Recipes from "./assets/Components/Recipes/Recipes"
+import { useEffect } from "react"
 
 function App() {
+  const [item, setItem] = useState([])
+
+  useEffect(() => {
+    fetch('/public/items.json')
+      .then(res => res.json())
+      .then(data => setItem(data))
+  }, [])
+  console.log(item)
 
   return (
     <>
