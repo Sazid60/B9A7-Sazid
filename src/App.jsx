414bedddd399de7dbfa0b136ca72e566bc8f5cf0
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-15 20:53:26 +0600

    Structure Made

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Banner from "./assets/Components/Banner/Banner"
+import Cart from "./assets/Components/Cart/Cart"
+import Navbar from "./assets/Components/Navbar/Navbar"
+import Recipes from "./assets/Components/Recipes/Recipes"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Recipes></Recipes>
+      <Cart></Cart>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Recipes/Recipes.jsx
@@ -0,0 +1,13 @@
+import Recipe from "../Recipe/Recipe";
+
+
+const Recipes = () => {
+    return (
+        <div>
+            <h1>Recipes</h1>
+            <Recipe></Recipe>
+        </div>
+    );
+};
+
+export default Recipes;
