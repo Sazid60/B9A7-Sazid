86bf7316f7b22747c7bb23a465be3ce020223dd8
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-16 23:11:30 +0600

    Font Fix For Mobile Device

--- src/assets/Components/Recipe/Recipe.jsx
+++ src/assets/Components/Recipe/Recipe.jsx
@@ -9,10 +9,10 @@ const Recipe = ({item,handleCookClick}) => {
             <div className="bg-base-100 shadow-xl p-3 rounded xl">
                 <figure><img className="rounded-xl h-40 w-full" src={recipe_image} alt="n/a" /></figure>
                 <div className="mt-4 space-y-4 px-2">
-                    <h2 className="text-xl font-bold">{recipe_name}</h2>
-                    <p className="text-[#878787]">{short_description}</p>
-                    <h1 className="font-bold text-xl">Ingredients : {ingredients.length}</h1>
-                    <ul className="list-disc pl-4 text-lg">
+                    <h2 className="text-lg lg:text-xl font-bold">{recipe_name}</h2>
+                    <p className="text-[#878787] text-sm lg:text-lg">{short_description}</p>
+                    <h1 className="font-bold text-sm lg:text-lg">Ingredients : {ingredients.length}</h1>
+                    <ul className="list-disc pl-4 text-sm lg:text-lg">
                         {
                             ingredients.map(ingredient=><li key={ingredient}>{ingredient}</li>)
                         }
@@ -20,15 +20,15 @@ const Recipe = ({item,handleCookClick}) => {
                     <div className="flex justify-around gap-2">
                         <div className="flex justify-center items-center gap-2">
                             <CiClock2 />
-                            <p>{preparing_time} minutes</p>
+                            <p className="text-sm lg:text-lg">{preparing_time} minutes</p>
                         </div>
                         <div className="flex justify-center items-center gap-1">
                         <SlFire />
-                            <p>{calories} Calories</p>
+                            <p className="text-sm lg:text-lg">{calories} Calories</p>
                         </div>
                     </div>
                     <div className="card-actions justify-start">
-                        <button onClick={()=>handleCookClick(item)} className="btn bg-green-400 p-4 rounded-full text-white shadow-xl">Want to Cook</button>
+                        <button onClick={()=>handleCookClick(item)} className="btn bg-green-400 text-sm lg:text-lg rounded-full text-white shadow-xl">Want to Cook</button>
                     </div>
                 </div>
             </div>
